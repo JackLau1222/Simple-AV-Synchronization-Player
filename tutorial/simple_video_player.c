a5897eddf6ed13cbead2a0cffde64bc3a91d4b4b
Jack Lau <2366536135@qq.com>
2024-11-25 17:17:53 +0800

    add LICENSE comments

--- tutorial/simple_video_player.c
+++ tutorial/simple_video_player.c
@@ -8,6 +8,16 @@
  * SDL2 version 2.30.3
  *
  * Tested on MacOS 14.1.2, compiled with clang 14.0.3
+ * 
+ * This file is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
  */
 
 #include <SDL.h>
